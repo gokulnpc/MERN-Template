bbfb9432355b1bfdc8a7008604a35698c0c2b9c0
Gokuleshwaran Narayanan <gokulnpc@gmail.com>
2024-09-07 15:12:57 -0400

    s14

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ const { api } = require('./routers/api');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
+const session = require('express-session');
 require('dotenv').config();
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
+}
 
 // EXPRESS APP
 const app = express();
@@ -21,11 +30,6 @@ app.use((req, res, next) => {
 });
 
 // PASSPORT GOOGLE AUTH CONFIG
-const config = {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
-}
-
 const AUTH_OPTIONS = {
     clientID: config.CLIENT_ID,
     clientSecret: config.CLIENT_SECRET,
@@ -40,12 +44,28 @@ const verifyCallback = (accessToken, refreshToken, profile, done) => {
 }
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+passport.serializeUser((user, done) => done(null, user.id)); // Saving the session data in the cookie
+passport.deserializeUser((id, done) => done(null, id)); // Retrieving the session data from the cookie
+// app.use(cookieSession({
+//     name: 'session',
+//     keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+//     maxAge: 24 * 60 * 60 * 1000,
+//     secure: false,
+//     httpOnly: true
+// }));
+app.use(session({
+    secret: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000, secure: false, httpOnly: true }
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 
 // GOOGLE OAUTH ROUTES
 app.get('/auth/google', passport.authenticate('google', { scope: ['email'] }));
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failure', session: false }), (req, res) => {
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failure', session: true }), (req, res) => {
     res.redirect('/success');
 });
 
@@ -58,13 +78,17 @@ app.get('/failure', (req, res) => {
 });
 
 app.get('/auth/logout', (req, res) => {
-    res.send('You have been logged out.');
+    req.logout((err)=>{
+        if(err) return res.status(500).json({message: 'Failed to logout'});
+        res.redirect('/');
+    })
 });
 
 // Protected Route with Authentication and Permission
 function checkAuth(req, res, next) {
-    const token = req.headers['authorization'];
-    if (!token || token !== 'Bearer mytoken') {
+    console.log("Current User:", req.user);
+    const isLogged = req.isAuthenticated() && req.user;
+    if (!isLogged) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
     next();
@@ -72,9 +96,9 @@ function checkAuth(req, res, next) {
 
 function checkPermission(req, res, next) {
     const permission = req.headers['permission'];
-    if (!permission || permission !== 'admin') {
-        return res.status(403).json({ message: 'Forbidden' });
-    }
+    // if (!permission || permission !== 'admin') {
+    //     return res.status(403).json({ message: 'Forbidden' });
+    // }
     next();
 }
 
